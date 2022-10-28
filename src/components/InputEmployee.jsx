c2fd3ddfcb0fdf6a2587b12c4540b2cb559f9e16
Sara Baqla <missatrox44@gmail.com>
2022-10-28 12:48:16 -0500

    created components and psuedocode

---
+++ src/components/InputEmployee.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function InputEmployee() {
+  return (
+    <div>
+      <p>Input Employee Component</p>
+    </div>
+  )
+}
+
+//enter name
+//enter title - server/trainee
---
+++ src/components/SmallTop.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function SmallTop() {
+  return (
+    <div>
+      <p>Small Top Component</p>
+    </div>
+  )
+}
+
+//row 
+//container w/ server/trainee name
+//total tables so far
+//buttons - +, skip, (break/back in the game), clock out
